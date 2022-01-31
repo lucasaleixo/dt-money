cebdf408e533f253daab264dfe1b35892cb743d6
root <root@LAPTOP-CNOT8HJN.localdomain>
2022-01-31 16:41:27 -0300

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,17 +4,14 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #e52e4d;
-        --blue: #e429cc;
-
+        --blue: #5429cc;
         --blue-light: #6933ff;
-
         --text-title: #363f5f;
         --text-body: #969cb3;
-
         --background: #f0f2f5;
         --shape: #ffffff;
     }
-
+    
     * {
         margin: 0;
         padding: 0;
@@ -26,7 +23,6 @@ export const GlobalStyle = createGlobalStyle`
         @media (max-width: 1080px) {
             font-size: 93.75%; // 15px
         }
-
         @media (max-width: 720px) {
             font-size: 87.5%; // 14px
         }
