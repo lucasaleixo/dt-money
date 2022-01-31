2162697e5039f6ad835788e829066fd0b412ed0d
root <root@LAPTOP-CNOT8HJN.localdomain>
2022-01-31 14:05:54 -0300

    added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
