0252c78c8ddc09ae4ee91042c362deafe7ed4929
root <root@LAPTOP-CNOT8HJN.localdomain>
2022-02-01 10:37:32 -0300

    created fake api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, []);
+
     return (
         <Container>
             <table>
@@ -16,7 +23,7 @@ export function TransactionsTable() {
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000,00</td>
+                        <td className="deposit">R$15.000,00</td>
                         <td>Desenvolvimento</td>
                         <td>31/01/2022</td>
                     </tr>
